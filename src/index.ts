457e5990771211d19938a64a0c49bdef558ea453
josezini-beep <jose.zini@plinenergia.com.br>
2026-09-24 15:17:57 -0300

    inicio typescript

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+let age: number = 2;
+const fisrstname: string = "felipe";
+const isValid : boolean = true;
+let idk: any = 5;
+idk = true
+
+const ids:[1,2,3,4,`a`];
+const booleans: boolean[]=[true,false,false];
+const names: string[]=["felipe","joana"];
+
+const person:[number, string]= [1,"2"]
+
+const people: [number,string][]=[
+    [1,"jane"],
+    [2,"doe"],
+];
+const productId:string|number = "1"
+
+console.log (age);
