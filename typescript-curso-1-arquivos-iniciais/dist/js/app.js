180d4f7c63844c3f75a68f7b50cd34a08a7de65d
josezini-beep <jose.zini@plinenergia.com.br>
2026-09-25 16:58:58 -0300

    atualização typescript

--- typescript-curso-1-arquivos-iniciais/dist/js/app.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/app.js
@@ -1,5 +1,14 @@
-import { Negociacao } from './models/negociacao';
+import { Negociacao } from './models/negociacao.js';
+import { NegociacaoController } from './controllers/negociacao-controller.js';
 const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao);
-console.log(negociacao.volume);
-alert("oi");
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+}
+else {
+    console.error('Formulário .form não encontrado no HTML.');
+}
